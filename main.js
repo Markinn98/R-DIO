103389c8c8504b68a96b9476800318354b902505
Prof. MARCOS ALEXANDRE <126907562+Markinn98@users.noreply.github.com>
2023-10-25 18:55:19 -0300

    21

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 function tocasomAplausos(){
-    document.querySelector("#som_tecla_aplausos").play();
+    document.querySelector("idElementoAudio").play();
 }
 const ListaDeTeclas = document.querySelectorAll(".tecla");
 ListaDeTeclas[0].onclick = tocasomAplausos;
 
 let contador = 0;
 
-while (contador<9){
+while (contador<ListaDeTeclas.length){
     ListaDeTeclas[contador].onclick=tocasomAplausos;
     contador=contador+1;
     console.log(contador);
