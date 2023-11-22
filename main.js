af4b8cc2e7761f6c33f7ef8adc2d649410d671fc
Prof. MARCOS ALEXANDRE <126907562+Markinn98@users.noreply.github.com>
2023-11-22 19:03:27 -0300

    .

--- main.js
+++ main.js
@@ -9,9 +9,17 @@ while (contador<ListaDeTeclas.length){
     const tecla = ListaDeTeclas [contador];
     const efeito = tecla.classList[1]
     const idAudio = `#som_${efeito}`;
+    
     tecla.onclick = function () {
     tocaSom(idAudio);
     }
     contador=contador+1;
+}
+tecla.onkeydown = function(){
+    tecla.classList.add('ativa');
+}
+
+tecla.onkeyup = function(){
+    tecla.classList.remove('ativa');
 }
 
